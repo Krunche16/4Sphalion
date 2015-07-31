8b0865d4f1fb163c0eb34d1c15c7cb12655eba5a
Krunche <krunche16@gmail.com>
2015-07-31 18:55:38 +0200

    Addition of the array `args` to controllers

--- src/app/controller/Controller.php
+++ src/app/controller/Controller.php
@@ -2,7 +2,27 @@
 namespace Sphalion\App\Controller;
 
 class Controller{
-	public function render($template, $title, $content = '', $status = '', $nav = ''){
+	protected $args = [];
+	
+	public function __construct(){
+		if(!empty($_POST)){
+			foreach ($_POST as $key => $value) {
+				$this->args['POST'][$key] = $value;
+			}
+		}else{
+			$this->args['POST'] = null;
+		}
+		
+		if(strpos(REQUEST, '/', 1) && substr(REQUEST, strpos(REQUEST, '/', 1)+1)){
+			$this->args = substr(REQUEST, strpos(REQUEST, '/', 1), strlen(REQUEST));
+			$this->args = trim($this->args, '/');
+			$this->args = explode('/', $this->args);
+		}else{
+			$this->args['GET'] = null;
+		}
+	}
+	
+	protected function render($template, $title, $content = '', $status = '', $nav = ''){
 		if($status !== ''){
 			ob_start();
 				include ROOT . '/src/app/views/templates/items/' . $status;
