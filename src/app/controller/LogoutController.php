485937aaaa850f7651cc12ef38185c45c61cf846
Krunche <krunche16@gmail.com>
2015-08-13 16:56:17 +0200

    Make LogoutController working

--- src/app/controller/LogoutController.php
+++ src/app/controller/LogoutController.php
@@ -2,7 +2,8 @@
 namespace Sphalion\App\Controller;
 
 class LogoutController extends Controller{
-	public function run(){
+	
+	public function index(){
 		session_destroy();
 		header('Location: Home');
 		exit();
